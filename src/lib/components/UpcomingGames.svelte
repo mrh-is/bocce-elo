e7b385bf342604e156a9b93bff5219408477b095
Michael Helmbrecht <me@mrh.is>
2026-06-28 12:18:09 -0400

    refactor(components): add UpcomingGames component

---
+++ src/lib/components/UpcomingGames.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { UpcomingGame } from "$lib/types.js";
+  import GameLine from "./GameLine.svelte";
+
+  const { games }: { games: UpcomingGame[] } = $props();
+</script>
+
+{#if games.length > 0}
+  {#each games as game (game.opponent)}
+    <GameLine court={game.court} opponent={game.opponent} prob={game.prob} />
+  {/each}
+{:else}
+  <span class="muted">–</span>
+{/if}
+
+<style>
+  .muted {
+    color: #253025;
+  }
+</style>
